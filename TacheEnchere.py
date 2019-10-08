c670f7635381ceace3e3ebc73941d3c20965f605
Lucas CROTTI <lucas.crotti@etudiant.univ-reims.fr>
2019-10-08 12:08:05 +0200

    fin seance

--- TacheEnchere.py
+++
@@ -1,7 +0,0 @@
-from Tache.py import *
-
-class TacheEnchere(Tache) :
-
-    def __init__(self, vEnchere):
-        Tache.__init__(self)
-        self.vEnchere = vEnchere
--- TacheSimple.py
+++
@@ -1,6 +0,0 @@
-from Tache.py import *
-
-class TacheSimple(Tache) :
-
-    def __init__():
-        Tache.__init__(self)
