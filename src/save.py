dde39100958e035f5d2fd93aaa16e65411d97d0b
devi0016 <orelian.devillers@etudiant.univ-reims.fr>
2019-11-05 16:26:16 +0100

    création json et commentaire de save

--- src/save.py
+++ src/save.py
@@ -8,40 +8,57 @@ from Carte import Carte
 
 class Save:
 
-    #carte
-    def importer(nomCarte):
-        chemin = os.path.join(curr_dir , nomCarte)
-        with open(chemin+'/taille.json' , 'r') as json_data:
-            Taille = json.load(json_data)
-            x = Taille["x"]
-            y = Taille["y"] 
+   
+    def importer(carte:Carte):
+        """
+        Transformation du contenue des fichiers json compris dans le dossier 'nomCarte' en objet carte
+        avec la liste des obstacles remplies , la taille et son nom definie
+        :parametre: une carte a remplir et deja enregistrer
+        :return: un objet carte remplit
+        """
+
+        chemin = os.path.join(curr_dir , carte.nom)
+        if (os.exists(chemin)):
+            with open(chemin+'/taille.json' , 'r') as json_data:
+                Taille = json.load(json_data)
+                carte.x = Taille["x"]
+                carte.y = Taille["y"] 
+                Obstacle = array()
+
+                with open(chemin+'/borne.json','r') as json_data:
+                    liste = json.load(json_data)
+                    for j in liste["Borne"]:
+                        b = Borne(j["id"] , j["pos1"] , j["pos2"])
+                        carte.listeObstacle.append(b)
+
+                with open(chemin+'/atelier.json','r') as json_data:
+                    liste = json.load(json_data)
+                    for j in liste["Atelier"]:
+                        b = Atelier(j["id"] , j["pos1"] , j["pos2"] , j["utilite"] , j["tache"])
+                        carte.listeObstacle.append(b)
+
+                with open(chemin+'/obstacle.json','r') as json_data:
+                    liste = json.load(json_data)
+                    for j in liste["Obstacle"]:
+                        b = Obstacle(j["id"] , j["pos1"] ,j["pos2"])
+                        carte.listeObstacle.append(b)
+
+                with open(chemin+'/Robot.json','r') as json_data:
+                    liste = json.load(json_data)
+                    for j in liste["Robot"]:
+                        b = Robot(j["id"] , j["transport"] , j["assemblage"]  , j["pos"] , j["vitesse"])
+                        carte.listeRobot.append(b)
         
-            Obstacle = array()
-            with open(chemin+'/borne.json','r') as json_data:
-                liste = json.load(json_data)
-                for j in liste["Borne"]:
-                    b = Borne(j["id"] , j["pos1"] , j["pos2"])
-                    Obstacle.append(b)
+    
+    def enregistrer(carte:Carte):
+        """
+        Transforme une carte et une liste de robot en fichier json
+        Prend en paramètre une carte  et la liste des robot dans la carte
+        :parametre: un objet carte 
+        """
 
-            with open(chemin+'/atelier.json','r') as json_data:
-                liste = json.load(json_data)
-                for j in liste["Atelier"]:
-                    b = Atelier(j["id"] , j["pos1"] , j["pos2"] , j["utilite"] , j["tache"])
-                    Obstacle.append(b)
-
-            with open(chemin+'/obstacle.json','r') as json_data:
-                liste = json.load(json_data)
-                for j in liste["Obstacle"]:
-                    b = Obstacle(j["id"] , j["pos1"] ,j["pos2"])
-                    Obstacle.append(b)
-
-        carte = carte(nomCarte , x , y , Obstacle)
-        return carte
-        
-
-    def enregistrer(carte , Robots):
         chemin = os.path.join(curr_dir)
-        if !(os.exists(chemin+carte.nom)):
+        if not(os.exists(chemin+carte.nom)):
             os.mkdir(os.path.join(curr_dir , nomCarte))
         chemin = os.path.join(curr_dir , nomCarte)
         o = []
@@ -54,13 +71,13 @@ class Save:
 
         for j in carte.listeObstacle:
             if type(j) is Borne :
-                b.append({ "id" : j.getid() , "pos1" : j.getPos1() , "pos2" : j.getPos2()})
+                b.append({ "id" : j.id , "pos1" : j.pos1 , "pos2" : j.pos2})
                 
             if type(j) is Atelier :
-                a.append({ "id" : j.getid() , "pos1" : j.getPos1() , "pos2" : j.getPos2() , "utilite" : j.getUtilite() , "tache" : j.getTache()})
+                a.append({ "id" : j.id , "pos1" : j.pos1 , "pos2" : j.pos2 , "utilite" : j.utilite , "tache" : j.tache})
 
             else :
-                o.append({ "id" : j.getid() , "pos1" : j.getPos1() , "pos2" : j.getPos2()})
+                o.append({ "id" : j.id , "pos1" : j.pos1 , "pos2" : j.pos2})
 
         with open(chemin+"/obstacle.json" , "w+") as json_data:
             obstacles = dict({"Obstacle" : o})
@@ -75,21 +92,9 @@ class Save:
             json.dump(ateliers, json_data , indent=4)
         
         r = []
-        for j in robot:
+        for j in carte.listeRobot:
             r.append({"id" : j.id , "transport" : j.transport , "assemblage" : j.assemblage , "pos" : j.pos , "vitesse" : j.vitesse})
-        with open(chemin+"/Robot.json" , "w+") as json_data:
+        with open(chemin+"/robot.json" , "w+") as json_data:
             robots = dict({"Robot" : r})
             json.dump(robots, json_data , indent=4)
         
-
-    #Robot
-    def importer(nomCarte): 
-        chemin = os.path.join(curr_dir , nomCarte)
-        robot = array()
-            with open(chemin+'/Robot.json','r') as json_data:
-                liste = json.load(json_data)
-                for j in liste["Robot"]:
-                    b = Robot(j["id"] , j["transport"] , j["assemblage"]  , j["pos"] , j["vitesse"])
-                    robot.append(b)
-        return robot
-    
