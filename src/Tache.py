aefec0c46999f40b9beaefdc632f30976ec96527
Adrades <genadrades@gmail.com>
2019-11-21 13:35:29 +0100

    Changement du __init__ de Tache

--- src/Tache.py
+++ src/Tache.py
@@ -3,20 +3,36 @@ import random
 
 class Tache:
 
-    def __init__(self, pos_depart:tuple, valid:bool = True ):
-        self.valid = valid  # Si la tâche est utilisable ou non (initialisation du robot)
+    def __init__(self, **kwargs):
+        """
+        Les arguments passent en kwargs pour éviter de faire des héritages pour 2 attributs
+        :param kwargs: "valid", "pos_fin", "pos_depart"
+        """
+        if "valide" in kwargs:
+            self.valide = kwargs["valide"]  # Si la tâche est utilisable ou non (initialisation du robot)
+        else:
+            self.valide = False
+
+        if "pos_fin" in kwargs:
+            self.fin = kwargs["pos_fin"]
+            self.type = "Transport"
+        else:
+            self.fin = (0, 0)
+            self.type = "Assemblage"
+
+        if "pos_depart" in kwargs:
+            self.depart = kwargs["pos_depart"]  # Si la tâche est utilisable ou non (initialisation du robot)
+        else:
+            self.depart = (0, 0)
+
+
         self.points = random.randint(1, 1000)  # à changer plus tard, la c'est juste pour test
         self.duree = random.randint(50, 200)  # Durée en secondes
         self.enchere = False
         self.Tfin = 0
-        self.depart = pos_depart
         # on dit qu'une tâche a 10% de chance d'être une enchère
         if random.randint(0,100) <= 10:
             self.enchere = True
-        if random.randint(0,100) <= 50:
-            self.type= "Assemblage"
-        else:
-            self.type = "Transport"
 
     def getPoints(self):
         return self.points
@@ -50,7 +66,7 @@ class Tache:
         return amende
 
     def __bool__(self):
-        return self.valid
+        return self.valide
 
     def __str__(self):
         return f"Points : {self.points} , durée : {self.duree} secondes , type : {self.type}"
