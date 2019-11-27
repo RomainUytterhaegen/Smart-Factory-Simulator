7d3f4b8941a361d14feb6ab5ca4e7e9d0d18ce23
Lucas CROTTI <lucas.crotti@etudiant.univ-reims.fr>
2019-11-27 10:19:03 +0100

    pas reussi message

--- src/gui/Test _message.py
+++ src/gui/Test _message.py
@@ -3,14 +3,18 @@ from tkinter import *
 
 class Test(Frame):
     def __init__(self, window):
-        Frame.__init__(self, window, bg='red', width=70000)
-        window.grid_propagate(0)
-        window.title("Message cadré")
-        self.pack()
+        Frame.__init__(self, window, bg='red')
 
-        self.message = Message(self, text="coucou", width=30000, justify="left")
-        self.message.pack()
+        window.rowconfigure(0, weight=1)
+        window.columnconfigure(0, weight=1)
 
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.grid(row=0, column=0, sticky='news')
+
+        self.message_textmode = Message(self, text="Coucou", width=100, justify='left',
+                                        highlightcolor='blue')
+        self.message_textmode.grid(row=0, column=0, padx=20, pady=20, sticky='news')
 
 root = Tk()
 test = Test(root)
