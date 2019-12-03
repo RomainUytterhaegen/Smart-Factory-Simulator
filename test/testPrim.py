f2483e6261cfa3aaf7a8f86408bf63a9d4913f94
Romain UYTTERHAEGEN <romain.uytterhaegen@etudiant.univ-reims.fr>
2019-12-03 16:20:16 +0100

    test after

---
+++ test/testPrim.py
@@ -0,0 +1,35 @@
+
+def heuristic(a:tuple, b:tuple):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def creer_graphe(*tuples):
+    """
+    Créer un graphe non orientée en matrice à partir des positions passées en paramètres.
+    Premier tuple : lieu de départ
+    Dernier tuple : lieu d'arrivée
+    :param tuples:
+    :return:
+    """
+
+    #INITIALISATION MATRICE A 0
+    graphe = [[0 for column in range(len(tuples))]
+                    for row in range(len(tuples))]
+
+    #REMPLISSAGE DES POIDS DE LA MATRICE
+    for i in range(len(tuples)):
+        for j in range(len(tuples)):
+            if i != j:
+                graphe[i,j] = heuristic(tuples[i],tuples[j])
+
+    return graphe
+
+def min_spanning_tree(graphe:list):
+    """
+    Créer l'arbre de poids minimum du graphe, ce qui permet dde déterminer le chemin le plus optimisé.
+    :param graphe:
+    :return:
+    """
+    pass
+
